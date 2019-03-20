e393d1b97814103e2c2ac21a7801d38d67ad9bbb
tourbillion42 <tourbillion42@gmail.com>
2019-03-20 22:26:07 +0900

    add gender and married function

--- CustomPlist/ListViewController.swift
+++ CustomPlist/ListViewController.swift
@@ -14,17 +14,44 @@ class ListViewController : UITableViewController, UIPickerViewDelegate, UIPicker
     @IBOutlet var name: UILabel!
     @IBOutlet var gender: UISegmentedControl!
     @IBOutlet var married: UISwitch!
+    
+    var accountList = [String]()
+    var defaultPlist : NSDictionary!
   
     override func viewDidLoad() {
+        
+        if let defaultPlistPath = Bundle.main.path(forResource: "UserInfo", ofType: "plist") {
+            self.defaultPlist = NSDictionary(contentsOfFile: defaultPlistPath)
+        }
         
     }
     
     @IBAction func changeGender(_ sender: UISegmentedControl) {
         
+        let val = sender.selectedSegmentIndex
+        let customPlist = "\(self.account.text!).plist"
+        
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let path = paths[0] as NSString
+        let plist = path.strings(byAppendingPaths: [customPlist]).first!
+        let data = NSMutableDictionary(contentsOfFile: plist) ?? NSMutableDictionary(dictionary: self.defaultPlist)
+        
+        data.setValue(val, forKey: "gender")
+        data.write(toFile: plist, atomically: true)
     }
     
     @IBAction func changeMarried(_ sender: UISwitch ) {
-     
+        
+        let val = sender.isOn
+        let customPlist = "\(self.account.text!).plist"
+        
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let path = paths[0] as NSString
+        let plist = path.strings(byAppendingPaths: [customPlist]).first!
+        let data = NSMutableDictionary(contentsOfFile: plist) ?? NSMutableDictionary(dictionary: self.defaultPlist)
+        
+        data.setValue(val, forKey: "married")
+        data.write(toFile: plist, atomically: true)
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -32,20 +59,25 @@ class ListViewController : UITableViewController, UIPickerViewDelegate, UIPicker
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        <#code#>
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
+        return self.accountList.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        
+        return self.accountList[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        <#code#>
+        
+        let val = self.accountList[row]
+        self.account.text = val
+        
+        let plist = UserDefaults.standard
+        plist.set(val, forKey: "selectedAccount")
+        plist.synchronize()
     }
-    
 }
 
